a5e65671e649d0eefed8881b363da179ce3f80e7
henintsoa454 <henintsoa404@gmail.com>
2023-07-10 20:56:24 +0430

    update back office

--- application/views/backoffice/home/index.php
+++ application/views/backoffice/home/index.php
@@ -8,7 +8,7 @@
         <meta name="author" content="" />
         <title>Admin Page</title>
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
-        <link href="css/styles.css" rel="stylesheet" />
+        <link rel="stylesheet" href="<?php echo base_url("assets/css/style.css"); ?>">
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     </head>
     <body class="sb-nav-fixed">
@@ -85,11 +85,11 @@
             </div>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="js/scripts.js"></script>
+        <script src="<?php echo base_url("assets/js/scripts.js"); ?>"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-        <script src="assets/demo/chart-area-demo.js"></script>
-        <script src="assets/demo/chart-bar-demo.js"></script>
+        <script src="<?php echo base_url("assets/demo/chart-area-demo.js"); ?>"></script>
+        <script src="<?php echo base_url("assets/demo/chart-bar-demo.js"); ?>"></script>
         <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
-        <script src="js/datatables-simple-demo.js"></script>
+        <script src="<?php echo base_url("assets/js/datatables-simple-demo.js"); ?>"></script>
     </body>
 </html>
