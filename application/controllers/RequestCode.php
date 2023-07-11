c956b6cf1a28ba6fbb7ead327de32e3dea32120e
Aris1401 <62713580+Aris1401@users.noreply.github.com>
2023-07-11 06:07:11 +0200

    changes

--- application/controllers/RequestCode.php
+++ application/controllers/RequestCode.php
@@ -2,10 +2,11 @@
     class RequestCode extends CI_Controller{
         public function sendRequestCode() {
             $this->load->model("Code_Model","code");
+            $this->load->model("Utilisateur_Model","Utilisateur");
             session_start();
             $code = $this->input->get('code');
             echo var_dump($_SESSION['current_user']);
-            $idUtilisateur = $_SESSION['current_user']->idUtilisateur;
+            $idUtilisateur = $_SESSION['current_user']->getIdUtilisateur();
             $this->code->requestToValidCode($idUtilisateur,$code);
             redirect(base_url("Profil"));
         }
