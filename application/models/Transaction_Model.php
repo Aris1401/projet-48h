f538c80932f2cf8966d0ba953742b47716c77989
Iantenaina <119032741+Iantenaina@users.noreply.github.com>
2023-07-11 04:46:46 +0200

    transaction

--- application/models/Transaction_Model.php
+++ application/models/Transaction_Model.php
@@ -56,6 +56,67 @@
      {
          return $this->idUtilisateur;
      }
+<<<<<<< Updated upstream
+=======
+
+     public function ajouter($sortie,$entree,$idUtilisateur)
+    {
+        $data = array(
+            'dateTransaction' => date('Y-m-d'),
+            'sortie' => $sortie,
+            'entre' => $entree,
+            'idUtilisateur' => $idUtilisateur
+        );
+
+        $this->db->insert('Transaction', $data);
+    }
+//////////////////////////////////////////////////////////////////////////
+    public function getTransactionByIdUtilisateur($idUtilisateur){
+    
+        $table_name = 'Transaction';
+
+        $query = "SELECT * FROM ".$table_name."WHERE idUtilisateur =  %i";
+        $query = sprintf($query, $this->db->escape($idUtilisateur));
+
+        $resultat = $this->db->query($query);
+        $results = array();        
+
+        if($resultat != null){
+            foreach ($resultat->result_array() as $data) {
+                array_push($results,$data);
+            }
+        }
+        else{
+            throw new Exception("Code validé par l'utilisateur sélectionné inexistant");
+        }
+        return $results;
+    }
+////////////////////////////////////////////////////////////////////////////////
+public function  InsertEntre($idUtilisateur,$dateTransaction,$entre)
+{
+    $query = "INSERT INTO Transaction (dateTransaction,sortie,entre,idUtilisateur) VALUES (%s, %s, %s, %s)";
+    $query = sprintf($query, $this->db->escape($designationRegime),
+     $this->db->escape($description), $this->db->escape($image), $this->db->escape($duree))
+     $this->db->escape($variationPoids), ;    
+    $this->db->query($query);
+
+    return $this->db->insert_id();
+} 
+/////////////////////////////////////////////////////////////////////////////////
+public function  InsertSortie($idUtilisateur,$dateTransaction,$entre)
+{
+    $query = "INSERT INTO Transaction (dateTransaction,sortie,entre,idUtilisateur) VALUES (%s, %s, %s, %s)";
+    $query = sprintf($query, $this->db->escape($designationRegime),
+     $this->db->escape($description), $this->db->escape($image), $this->db->escape($duree))
+     $this->db->escape($variationPoids), ;    
+    $this->db->query($query);
+
+    return $this->db->insert_id();
+}
+/////////////////////////////////////////////////////////////////////////////////
+
+
+>>>>>>> Stashed changes
  }
  
 ?>
